481969683ebc2be2c2ea378c244b45db21f9f02d
Nate S <seilaffn@gmail.com>
2022-08-09 15:37:55 -0600

    initial commit

---
+++ Methods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsExercise
+{
+    internal class Methods
+    {
+        public static int Add(int x, int y)
+        {
+            return (x + y);
+        }
+        public static int Sub(int x, int y)
+        {
+            return (x - y);
+        }
+        public static int Multiply(int a, int b)
+        {
+            return (a * b);
+        }
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public static int Mod(int a, int b)
+        {
+            return a % b;
+        }
+        public static double AreaOfCircle(double r)
+        {
+             var answer =  Math.PI * Math.Pow(r, 2);
+            return Math.Round(answer, 2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OperatorsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Methods.Add(5, 5));
+            Console.WriteLine(Methods.Sub(20, 6));
+            Console.WriteLine(Methods.Multiply(15, 6));
+            Console.WriteLine(Methods.Divide(27, 5));
+            Console.WriteLine(Methods.Mod(27, 5));
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a} / {b} = {quotient} with a remainder of {remainder}" );
+            }
+
+            Console.WriteLine("Put in the size of you circle");
+            var userInput = double.Parse(Console.ReadLine());
+            Console.WriteLine(Methods.AreaOfCircle(userInput));
+        }
+    }
+}
